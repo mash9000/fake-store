3a393b5a5e15ac8ae93ff67e1e466c4c7c24383a
simmetr <vm.certo@gmail.com>
2026-02-16 00:38:36 +0700

    temp remove 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vitest/config'
+import {defineConfig} from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
     test: {
-        environment: 'jsdom',
-        globals: true,
-        // setupFiles: './setupTest.ts',
-    },
-    build: {
-        outDir: 'build'
+        environment: "jsdom", pool: "threads", // или "vm"
+        poolOptions: {
+            threads: {
+                singleThread: true
+            }
+        },
+        build: {
+            outDir: 'build'
+        }
     }
 })
